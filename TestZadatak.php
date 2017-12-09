f1c77136f241b4716a076a3e6b9538e437d4a3dd
Filip <filip.kuc94@gmail.com>
2017-12-09 21:36:31 +0100

    Ucitavanje

--- TestZadatak.php
+++ TestZadatak.php
@@ -1,12 +1,8 @@
 <?php
-include 'vendor/autoload.php';
-use Sabre\Xml\Service;
 include 'UcitajPodatke2.php';
-//$test = new UcitajPodatke2;
-
 $test = new UcitajPodatke2();
-$test2 = 'zadatak1.xml';
-echo $test->executeXml($test2);
+/* $test2 = 'zadatak1.xml';
+echo $test->executeXml($test2); */
 
 
 
--- UcitajPodatke2.php
+++ UcitajPodatke2.php
@@ -5,11 +5,7 @@
  * Date: 28.11.2017.
  * Time: 11:09
  */
-
 include 'vendor/autoload.php';
-include 'Pages.php';
-include 'Page.php';
-
 use Sabre\Xml\Service;
 
 class UcitajPodatke2
@@ -20,7 +16,14 @@ class UcitajPodatke2
     public function __construct()
     {
         $this->service = new Sabre\Xml\Service();
+    }
 
+    public function executeXml($filename)
+    {
+        $varijabla = $this->citajXml($filename);
+        $parsanixml = $this->parseXml($varijabla);
+        $this->ispisXml($parsanixml);
+        return $parsanixml;
     }
 
     public function citajXml($filename)
@@ -31,71 +34,15 @@ class UcitajPodatke2
 
     public function parseXml($xml)
     {
-        /*
-      $this->service->elementMap = [
-
-
-          '{zadatak1.xml}page' => function(Sabre\Xml\Reader $reader) {
-              $pages = new Pages();
-              $children = $reader->parseInnerTree();
-              foreach($children as $child) {
-                  if ($child['value'] instanceof page) {
-                      $pages->pages[] = $child['value'];
-                  }
-              }
-              return $pages;
-          },
-          '{zadatak1.xml}page' => function(Sabre\Xml\Reader $reader) {
-
-              $page = new Page();
-              $keyValue = Sabre\Xml\Deserializer\keyValue($reader, 'zadatak1.xml');
-
-              if (isset($keyValue['title'])){
-                  $page->title = $keyValue['title'];
-              }
-               if (isset($keyValue['author'])){
-                  $page->author = $keyValue['author'];
-               }
-               return $page;
-          }
-
-       ];
-          */
-
-        $this->service->elementMap = [
-            '{zadatak1.xml}page' => function(Sabre\Xml\Reader $reader) {
-                return Sabre\Xml\Deserializer\keyValue($reader, 'zadatak1.xml');
-            },
-            '{zadatak1.xml}pages' => function(Sabre\Xml\Reader $reader) {
-                return Sabre\Xml\Deserializer\repeatingElements($reader, '{zadatak1.xml}page');
-            },
-
-
-
-        ];
-
         $parsiraj = $this->service->parse($xml);
         return $parsiraj;
     }
 
     public function ispisXml($output)
     {
-        /*
-        $service = new Sabre\Xml\Service();
-        */
         $ispis = print_r($output);
         $ispis = '<pre>' . $ispis . '</pre>';
-
         return $ispis;
-    }
-
-    public function executeXml($filename)
-    {
-
-        $varijabla = $this->citajXml($filename);
-        $parsanixml = $this->parseXml($varijabla);
-        $this->ispisXml($parsanixml);
-        return $parsanixml;
     }
 
 
